48eca54a4f257f0a361f074d54a76955ceeb9881
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-07-20 14:53:35 -0600

    Add errorLog function

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -2,4 +2,8 @@ const informationLog = (information) => {
   console.log(...information);
 };
 
-module.exports = informationLog;
+const errorLog = (information) => {
+  console.error(...information);
+};
+
+module.exports = { informationLog, errorLog };
