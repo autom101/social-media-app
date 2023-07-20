ec5becda4ccfd6b39e16c4f1993e72fe35ebd890
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-07-20 14:54:38 -0600

    Create error handler middleware

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,1 +1,8 @@
+const logger = require("./logger");
 
+const errorHandler = (error, request, response, next) => {
+  logger.errorLog(error);
+  next(error);
+};
+
+module.exports = { errorHandler };
