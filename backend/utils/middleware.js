a0a93b0e481257201e89bae01b3ed0474e8fb4c4
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-07-22 14:32:19 -0600

    Modify errorHandler middleware to respond with 400 if there is a validation error

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -12,11 +12,14 @@ const requestHandler = (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
   logger.errorLog(error);
+  if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
   next(error);
 };
 
 const unknownEndpoint = (error, request, response, next) => {
-  response.status(404).send({ error: "unknown endpoint" });
+  return response.status(404).send({ error: "unknown endpoint" });
 };
 
 module.exports = { requestHandler, errorHandler, unknownEndpoint };
