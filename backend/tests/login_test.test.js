7d5daa330369e3c0a491738bb9d1fee9cb297418
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-07-22 14:54:26 -0600

    Add a test that tests for logging in with correct credentials

--- backend/tests/login_test.test.js
+++ backend/tests/login_test.test.js
@@ -8,7 +8,37 @@ const User = require("../models/user");
 const api = supertest(app);
 
 beforeEach(async () => {
-  //...
+  //Have to empty db and create a user before running any tests.
+  await User.deleteMany({});
+  const passwordHash = await bcrypt.hash("12345", 10);
+  const newUser = new User({
+    username: "user_test",
+    name: "test user",
+    passwordHash,
+  });
+  await newUser.save();
+});
+
+describe("When logging in with one user in the database", () => {
+  test("client will receive 401 if an incorrect password is provided", async () => {
+    const user = {
+      username: "user_test",
+      password: "incorrect password",
+    };
+
+    await api.post("/api/login").send(user).expect(401);
+  });
+
+  test("users can successfully login with correct password and username", async () => {
+    const user = {
+      username: "user_test",
+      password: "12345",
+    };
+
+    const receivedData = await api.post("/api/login").send(user).expect(201);
+
+    expect(receivedData.body.token).toBeDefined();
+  });
 });
 
 afterAll(async () => {
