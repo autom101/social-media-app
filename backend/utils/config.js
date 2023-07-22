ef4d1d6a270cb2ba5a538295ec92860f26ce34f5
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-07-22 14:34:07 -0600

    Change MONGODB_URI variable so that it uses the test database if NODE_ENV is test

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3003;
 const SECRET_STRING = process.env.SECRET;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = { PORT, SECRET_STRING, MONGODB_URI };
