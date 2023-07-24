3fa19e94113d9aa32ffa3d8a90b2ef10d426017b
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-07-24 12:01:24 -0600

    Create form for logging in

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import loginService from "../services/login";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    loginService.login({ username, password });
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleLoginSubmit}>
         <label>Username: </label>
-        <input />
+        <input
+          id="username"
+          data-cy="login-username"
+          value={username}
+          name="username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
         <label>Password: </label>
-        <input />
+        <input
+          id="password"
+          data-cy="login-password"
+          value={password}
+          name="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">Submit</button>
       </form>
     </>
   );
