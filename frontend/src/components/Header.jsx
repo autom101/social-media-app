414a467852b54add6eb10baf97133cf325525d62
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-08-17 15:28:12 -0600

    Add styling to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,13 @@
+import PersonSharpIcon from "@mui/icons-material/PersonSharp";
+import "./css/Header.css";
+
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <h1>Apppppppp</h1>
+      <PersonSharpIcon className="header-avatar" />
+    </div>
+  );
 };
 
 export default Header;
