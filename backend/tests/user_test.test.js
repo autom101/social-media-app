767520ee949fe2045d693b112f79dc98ddc306ac
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-08-18 15:07:40 -0600

    Write initial test for /users route

--- backend/tests/user_test.test.js
+++ backend/tests/user_test.test.js
@@ -7,13 +7,28 @@ const User = require("../models/user");
 
 const api = supertest(app);
 
+//Empty database and initialize a test user.
 beforeEach(async () => {
-  //
+  await User.deleteMany({});
+  const passwordHash = await bcrypt.hash("abcde", 10);
+  const user = new User({
+    name: "testing_user",
+    username: "testing_username",
+    passwordHash,
+  });
+
+  await user.save();
 });
 
-test("Test", () => {
-  //
-});
+test("check that testing_username exists in the database and is the only user", async () => {
+  const result = await api
+    .get("/api/users")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const users = result.body;
+  expect(users).toHaveLength(1);
+}, 20000);
 
 afterAll(async () => {
   await mongoose.connection.close();
