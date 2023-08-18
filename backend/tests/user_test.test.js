d6bfee295662a844134579aef94e519b0f324fb8
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-08-18 15:56:15 -0600

    Add test to see that password validation rejects passwords that are not eight characters long and missing numbers or letters

--- backend/tests/user_test.test.js
+++ backend/tests/user_test.test.js
@@ -31,7 +31,7 @@ test("check that testing_username exists in the database and is the only user", 
 }, 10000);
 
 describe("with one user, testing_username, in the database", () => {
-  test("attempts to create a user with a non-unique username fails", async () => {
+  test("attempt to create a user with a non-unique username fails", async () => {
     const user = {
       name: "new_user",
       username: "testing_username",
@@ -39,7 +39,30 @@ describe("with one user, testing_username, in the database", () => {
     };
 
     const response = await api.post("/api/users").send(user).expect(400);
-    console.log(response.body);
+  });
+
+  test("attempt to create a user with a bad password fails", async () => {
+    const notEightChars = {
+      name: "new_user",
+      username: "username :)",
+      password: "1bcdefg",
+    };
+
+    const missingNumber = {
+      name: "new_user",
+      username: "username :)",
+      password: "Abcdefgasdf",
+    };
+
+    const missingLetter = {
+      name: "new_user",
+      username: "username :)",
+      password: "123456789",
+    };
+
+    await api.post("/api/users").send(notEightChars).expect(400);
+    await api.post("/api/users").send(missingNumber).expect(400);
+    await api.post("/api/users").send(missingLetter).expect(400);
   });
 }, 20000);
 
