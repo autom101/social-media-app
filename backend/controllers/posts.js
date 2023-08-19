a63f42e5fc19b4f7fca80e33f874d74069244fab
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-08-19 12:46:29 -0600

    Create get route to get all posts in postRouter

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,3 +1,13 @@
 const postRouter = require("express").Router();
+const Post = require("../models/post");
+
+postRouter.get("/", async (request, response, next) => {
+  try {
+    const posts = await Post.find({});
+    return response.json(posts);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = postRouter;
