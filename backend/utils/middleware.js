9b95f4a5e2c68460fe1e0c5d6ddbf58aacdb4e83
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-08-19 14:54:45 -0600

    Create userExtractor and tokenExtractor middleware

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const requestHandler = (request, response, next) => {
   const { path, method, body } = request;
@@ -7,6 +9,30 @@ const requestHandler = (request, response, next) => {
   logger.informationLog(`Method: ${method}`);
   logger.informationLog(`Body: `, body);
   logger.informationLog(`------------End Request Log------------`);
+  next();
+};
+
+const tokenExtractor = (request) => {
+  const authorization = request.get("authorization");
+  const authScheme = "Bearer ";
+  let token = null;
+  if (authorization && authorization.startsWith(authScheme)) {
+    token = authorization.replace(authScheme, "");
+  }
+  request.token = token;
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (decodedToken.username) {
+      const user = await User.findOne({ username: decodedToken.username });
+      request.user = user;
+    } else {
+      return response.status(401).json({ error: "invalid token" });
+    }
+  }
   next();
 };
 
@@ -22,4 +48,10 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
-module.exports = { requestHandler, unknownEndpoint, errorHandler };
+module.exports = {
+  requestHandler,
+  tokenExtractor,
+  userExtractor,
+  unknownEndpoint,
+  errorHandler,
+};
