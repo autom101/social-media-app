76e2111f58c0fa809b93e287c93b520a07ee8f5f
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-08-20 13:18:10 -0600

    Create a comment schema and model

---
+++ backend/models/comment.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+  },
+  edited: {
+    type: Boolean,
+  },
+  editedAt: {
+    type: Date,
+  },
+  likes: {
+    type: Number,
+  },
+});
+
+commentSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
