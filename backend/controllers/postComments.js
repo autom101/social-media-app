07aa37b326dafa22281231f85d993cc2e51445dd
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-08-20 16:43:15 -0600

    Create an endpoint to create a new comment

--- backend/controllers/postComments.js
+++ backend/controllers/postComments.js
@@ -14,4 +14,32 @@ postCommentRouter.get("/", async (request, response, next) => {
   }
 });
 
+//Extract comments array from post, then create a new comment and add it to the comment collection. Then, add the saved comment to post's comments Array and save that in the post collection.
+
+postCommentRouter.post("/", async (request, response, next) => {
+  try {
+    const id = request.params.postId;
+    const post = await Post.findById(id).populate("comments");
+    const { comments } = post;
+
+    const newComment = new Comment({
+      content: request.content,
+      createdAt: new Date().getTime(),
+      author: request.user,
+      edited: false,
+      editedAt: null,
+      likes: 0,
+    });
+
+    const savedComment = await newComment.save();
+
+    post.comments = { ...comments, savedComment };
+    const savedPost = await post.save();
+
+    return response.json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = postCommentRouter;
