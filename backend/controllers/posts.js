e86e07a779df3de02f09a2a7937227c5c5b59dce
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-08-20 16:24:05 -0600

    Create an endpoint that lets a user create a post

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -10,4 +10,32 @@ postRouter.get("/", async (request, response, next) => {
   }
 });
 
+postRouter.post("/", async (request, response, next) => {
+  try {
+    const user = request.user;
+    if (!user || user === null) {
+      return response
+        .status(403)
+        .json({ error: "You must be logged in to create a post." });
+    }
+
+    const newPost = new Post({
+      title: request.title,
+      author: user,
+      createdAt: new Date().getTime(),
+      comments: [],
+      likes: 0,
+      likedBy: [],
+      edited: false,
+      editedAt: null,
+    });
+
+    const savedPost = await newPost.save();
+
+    return response.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = postRouter;
