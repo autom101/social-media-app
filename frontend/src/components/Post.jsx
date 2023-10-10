fd4559dbb41ee246f0ef6c2a8a797352ed1f2142
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-10-10 15:47:45 -0600

    Create html code for a post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -2,7 +2,31 @@ import { useParams } from "react-router-dom";
 
 const Post = () => {
   const { id } = useParams();
-  return <div>Post {id}</div>;
+  return (
+    <article className="post-container">
+      <div className="post-info">
+        <img className="post-avatar" src="" alt="" />
+        <h3 className="post-username">Some random user {id}</h3>
+      </div>
+      {/*This is going to contains extra options such as share, save, etc*/}
+      <div className="post-extra"></div>
+      {/*This is going to contain any images and text in the post and when it was created*/}
+      <div className="post-content">
+        <p>
+          Whatever text is on the post that is eventually going to get cut off
+          at some point.
+        </p>
+        <h4 className="post-time-created">Posted this at: 6 pm</h4>
+      </div>
+      {/*This is going to contain the like button and comments*/}
+      <div className="post-options">
+        <button className="post-like-btn">Like</button>
+        <a className="post-comments-link" href="#">
+          This will lead to comments
+        </a>
+      </div>
+    </article>
+  );
 };
 
 export default Post;
