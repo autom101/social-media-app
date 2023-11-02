4bc3d9c3c3933a83acbb415376b0cf51da83fd82
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-11-02 10:03:13 -0600

    Add imports to MyPosts

--- frontend/src/components/MyPosts.jsx
+++ frontend/src/components/MyPosts.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPosts } from "../reducers/postReducer";
+
 const MyPosts = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch();
+  });
   return <div>MyPosts</div>;
 };
 
