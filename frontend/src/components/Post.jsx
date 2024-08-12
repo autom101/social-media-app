c8641082c42110e9151107ba42b16b2b6a4485b6
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2024-08-12 15:13:00 -0600

    Modify the Post page to use the Paper component

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,31 +1,36 @@
 /* eslint-disable react/prop-types */
 import "./css/Post.css";
+import { Paper } from "@mui/material";
 
 const Post = ({ post }) => {
-  console.log(post);
-
   return (
-    <article className="post-container flex">
-      <div className="post-info flex">
-        <img className="post-avatar" src="" alt="" />
-        <h3>Some random user</h3>
-      </div>
-      {/*This is going to contain extra options such as share, save, etc*/}
-      <div className="post-content flex">
-        <p>{post.title}</p>
-      </div>
-      {/* */}
-      <div className="post-extra flex">
-        <h4 className="post-time-created flex">
-          {new Date(post.createdAt).toLocaleTimeString("en-US")}
-        </h4>
-      </div>
-      {/*This is going to contain the like button and comments*/}
-      <div className="post-options flex">
-        <button className="post-like-btn">Like</button>
-        <a href="#">This will lead to comments</a>
-      </div>
-    </article>
+    <Paper
+      elevation={2}
+      component="article"
+      sx={{ backgroundColor: "#eefff3" }}
+    >
+      <article className="post-container flex">
+        <div className="post-info flex">
+          <img className="post-avatar" src="" alt="" />
+          <h3>Some random user</h3>
+        </div>
+        {/*This is going to contain extra options such as share, save, etc*/}
+        <div className="post-content flex">
+          <p>{post.title}</p>
+        </div>
+        {/* */}
+        <div className="post-extra flex">
+          <h4 className="post-time-created flex">
+            {new Date(post.createdAt).toLocaleTimeString("en-US")}
+          </h4>
+        </div>
+        {/*This is going to contain the like button and comments*/}
+        <div className="post-options flex">
+          <button className="post-like-btn">Like</button>
+          <a href="#">This will lead to comments</a>
+        </div>
+      </article>
+    </Paper>
   );
 };
 
