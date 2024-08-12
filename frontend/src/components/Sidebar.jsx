b2f6faefae0fc12cbff46c0dec8e10b710073c1a
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2024-08-12 14:25:48 -0600

    Replace sidebar tabs with MUI components

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,24 +1,45 @@
+/* eslint-disable react/prop-types */
 import "./css/Sidebar.css";
 import { Link } from "react-router-dom";
-import { Box, Tabs, Tab } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import GradeIcon from "@mui/icons-material/Grade";
+import SettingsIcon from "@mui/icons-material/Settings";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+
+const SidebarLink = ({ to, text, icon }) => {
+  return (
+    <ListItemButton component={Link} to={to}>
+      <ListItemIcon>{icon}</ListItemIcon>
+      <ListItemText primary={text} />
+    </ListItemButton>
+  );
+};
 
 const Sidebar = () => {
   return (
     <Box>
-      <Tabs orientation="vertical">
-        <Tab>
-          <Link to="/home">Home</Link>
-        </Tab>
-        <Tab>
-          <Link to="/home/myposts">My Posts</Link>
-        </Tab>
-        <Tab>
-          <Link to="/home/saved">Saved</Link>
-        </Tab>
-        <Tab>
-          <Link to="/home/settings">Settings</Link>
-        </Tab>
-      </Tabs>
+      <List aria-label="sidebar links">
+        <ListItem disablePadding>
+          <SidebarLink to="/home" text="Home" icon={<HomeIcon />} />
+        </ListItem>
+        <ListItem disablePadding>
+          <SidebarLink to="/myposts" text="My Posts" icon={<GradeIcon />} />
+        </ListItem>
+        <ListItem disablePadding>
+          <SidebarLink to="/saved" text="Saved" icon={<BookmarkIcon />} />
+        </ListItem>
+        <ListItem disablePadding>
+          <SidebarLink to="/settings" text="Settings" icon={<SettingsIcon />} />
+        </ListItem>
+      </List>
     </Box>
   );
 };
