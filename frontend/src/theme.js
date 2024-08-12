e16796c55025a4b6d968cf3d08c429bd38a72834
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2024-08-12 15:10:35 -0600

    Create a theme file

---
+++ frontend/src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      post: "#eefff3",
+      headingTitle: "-webkit-linear-gradient(#ec4899, #fb7185)",
+    },
+    text: {
+      username: "#00a89d",
+    },
+  },
+});
+
+export default theme;
