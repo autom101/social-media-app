a5ce0202cce8f5f4e23ca081f8b46740063a868d
autom101 <122489814+autom101@users.noreply.github.com>
2024-08-22 14:42:47 -0600

    Create a service method to like a post

---
+++ frontend/src/services/like.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const url = "http://localhost:3003/api/posts";
+
+const likePost = async (userInfo, postId) => {
+  const likeUrl = `${url}/${postId}/like`;
+  const response = await axios.post(likeUrl, {
+    headers: {
+      Authorization: `Bearer ${userInfo.token}`,
+    },
+  });
+
+  const data = await response.data;
+
+  console.log(data);
+
+  return data;
+};
+
+export default { likePost };
