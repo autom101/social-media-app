96009eb7ae25977ef46118b857f1d186f38dbe57
autom101 <122489814+autom101@users.noreply.github.com>
2024-08-22 15:56:13 -0600

    Revert "Update the likePost method to call the correct reducer function"
    This reverts commit ac6855bafa2e9a8cdafbafac17e9264f4aa0595c.

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,9 +1,4 @@
 /* eslint-disable react/prop-types */
-// redux and reducers imports
-import { useDispatch, useSelector } from "react-redux";
-import { updateLikedPost } from "../reducers/postReducer";
-
-// mui imports
 import {
   Card,
   CardHeader,
@@ -15,14 +10,11 @@ import {
 } from "@mui/material";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 
+const likePost = () => {
+  //
+};
+
 const Post = ({ post }) => {
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
-  const likePost = async () => {
-    dispatch(updateLikedPost(user, post.id));
-  };
-
   return (
     <Card
       component="article"
