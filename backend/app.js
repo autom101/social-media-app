536f6114fb71f62768648c450c3f3f2053038146
autom101 <122489814+autom101@users.noreply.github.com>
2024-08-23 13:15:00 -0600

    Add the cookie parser module

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const config = require("./utils/config");
 
 const logger = require("./utils/logger");
@@ -22,6 +23,7 @@ mongoose.connect(url);
 // Basic Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(middleware.requestHandler);
 
 // Routes
