0aadd63470a5a9ed594119a9f8fd5f0057400ee7
autom101 <122489814+autom101@users.noreply.github.com>
2024-09-05 09:28:31 -0600

    Add a content field to the post model

--- backend/models/post.js
+++ backend/models/post.js
@@ -7,6 +7,10 @@ const postSchema = new mongoose.Schema({
     unique: true,
     required: true,
   },
+  content: {
+    type: String,
+    required: true,
+  },
   author: {
     type: mongoose.Schema.Types.ObjectID,
     ref: "User",
